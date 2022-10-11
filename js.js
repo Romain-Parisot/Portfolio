5ed73075405f39a373e843e877a2e0022e00da3d
Rowars2 <romain.parisot@edu.devinci.fr>
2022-10-11 15:50:15 +0200

    🏗️totaly change nav bar

--- js.js
+++ js.js
@@ -2,8 +2,15 @@
 function toggleburger(){
     let header=document.querySelector('.header')
     let burger=document.querySelector('.burger')
+    let barUp=document.querySelector('.bar_up')
+    let barDown=document.querySelector('.bar_down')
+    let barMid=document.querySelector('.bar_mid')
     burger.addEventListener('click', ()=>{
-        header.classList.toggle('none');
+        barMid.classList.toggle('none');
+        barUp.classList.toggle('barAnimationTop');
+        barDown.classList.toggle('barAnimationBot');
+        header.classList.toggle('animationHeader');
+
     })
 }
 toggleburger();
